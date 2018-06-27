08d8f75d0e3eb7c5bfb89e092a0c748869a8c9d6
Sam Layton <slayton@uccs.edu>
2018-06-27 11:07:26 -0600

    w4v2
    + Corpse game object to be created when murderer kills a victim
    - Weapon class no longer holds possible causes of death. This is stored in the corpse object.
    - Removed Weapon prefab GameObject since these are only stored within code

---
+++ DeadMan'sParty/Assets/Scripts/Corpse and Murder Weapon Scripts/Corpse.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The remains of a murder victim
+/// </summary>
+public class Corpse : MonoBehaviour
+{
+    // corpse identification fields
+    CharacterList victimName = CharacterList.Baskerville;   // name of victim
+    GameObject currentRoom;                                 // room corpse was left in
+
+    // cause of death support fields
+    WeaponTypes wepType;                                // type of weapon used to kill victim and create corpse
+    List<string> causesOfDeath = new List<string>();    // collection of possible causes of death
+                                                        // Note: contents of list are dependant on type of weapon 
+                                                        // used to kill victim and create corpse
+
+    /// <summary>
+    /// Grants read and write access to name of victim
+    /// </summary>
+    public CharacterList VictimName
+    {
+        get { return victimName; }
+        set { victimName = value; }
+    }
+
+    /// <summary>
+    /// Grants read and write access to room
+    /// corpse was left in
+    /// </summary>
+    public GameObject CurrentRoom
+    {
+        get { return currentRoom; }
+        set { currentRoom = value; }
+    }
+
+    /// <summary>
+    /// Grants write access to type of weapon used on victim
+    /// Alters possible causes of death based on value
+    /// </summary>
+    public WeaponTypes WeaponTypeUsed
+    {
+        set
+        {
+            // sets weapon type to entered value
+            wepType = value;
+
+            // loads in possible causes of death
+            causesOfDeath.Clear();
+            switch (wepType)
+            {
+                // adds blunt weapon damage
+                case WeaponTypes.blunt:
+                    causesOfDeath.Add("bruising");
+                    causesOfDeath.Add("bone fractures");
+                    causesOfDeath.Add("blunt force trauma");
+                    break;
+                // adds poison weapon damage
+                case WeaponTypes.poison:
+                    causesOfDeath.Add("bloodshot eyes");
+                    causesOfDeath.Add("swollen muscles");
+                    causesOfDeath.Add("bluish lips");
+                    break;
+                // adds slashing weapon damage
+                case WeaponTypes.slashing:
+                    causesOfDeath.Add("lacerations");
+                    causesOfDeath.Add("deep cuts");
+                    break;
+                // adds stabbing weapon damage
+                case WeaponTypes.stabbing:
+                    causesOfDeath.Add("penetrating trauma");
+                    causesOfDeath.Add("cavitation");
+                    causesOfDeath.Add("stab wounds");
+                    break;
+                // adds strangulation weapon damage
+                case WeaponTypes.strangulation:
+                    causesOfDeath.Add("neck abrasions");
+                    causesOfDeath.Add("ligature marks");
+                    causesOfDeath.Add("swollen tongue");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Returns a random cause of death
+    /// </summary>
+    public string CauseOfDeath
+    {
+        get
+        {
+            int randCOD = Random.Range(0, causesOfDeath.Count);
+            return causesOfDeath[randCOD];
+        }
+    }
+}
---
+++ DeadMan'sParty/Assets/Scripts/Corpse and Murder Weapon Scripts/Weapon.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A weapon used by a murderer
+/// Note: This IS NOT instantiated as a game object.
+/// Rather, it should be created and stored within room
+/// and murderer scripts.
+/// </summary>
+public class Weapon
+{
+    string weaponName;                                      // unique name of the weapon
+    WeaponTypes type;                                       // universal type of weapon
+
+    /// <summary>
+    /// Constructor for the weapon class
+    /// </summary>
+    /// <param name="wepName">name of new weapon</param>
+    /// <param name="wepType">universal type of new weapon</param>
+    public Weapon (string wepName, WeaponTypes wepType)
+    {
+        // sets name and type of weapon
+        weaponName = wepName;
+        type = wepType;
+    }
+
+    /// <summary>
+    /// Returns name of weapon
+    /// </summary>
+    public string WeaponName
+    {
+        get { return weaponName; }
+    }
+
+    /// <summary>
+    /// Returns weapon's type
+    /// </summary>
+    public WeaponTypes Type
+    {
+        get { return type; }
+    }
+}
